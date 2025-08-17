cb0beb86ebd5efbb21f180266e7aba9df40a3aaa
3tal3mat3 <87993535+ndzy@users.noreply.github.com>
2025-08-17 20:11:12 +0500

    +Декларативная конфигурация для прав пользователя и автологин.

--- nixos/user.nix
+++ nixos/user.nix
@@ -1,7 +1,19 @@
-{ pkgs, user, ... }: {
+{ pkgs, ... }: 
+{
+  
   programs.fish.enable = true;
 
   users = {
+    
     defaultUserShell = pkgs.fish;
+  
+    users.sicsick = {
+
+        isNormalUser = true;
+        description = "sicsick";
+        extraGroups = [ "networkmanager" "wheel" "input" ];
+    };
   };
+
+  services.getty.autologinUser = "sicsick";
 }
