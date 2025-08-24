c3991e915c46dfee192651426d603c4027305156
ndzy <3tal3mat3@gmail.com>
2025-08-24 19:59:23 +0500

    + Флейки

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     
     home-manager = {
     
-        url = "github:nix-community/home-manager"
+        url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
     
     };
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -16,6 +16,13 @@
 	    ./steam.nix
     
     ];
+  
+  nix.settings.experimental.features = [ 
+  
+  "nix-command" 
+  "flakes" 
+  
+  ];
 
   system.stateVersion = "25.05"; # Did you read the comment?
 
