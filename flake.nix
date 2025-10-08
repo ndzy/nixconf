aeaa5e672b5bf3a6d9ce232bdf9107782848e482
3tal3mat3 <87993535+ndzy@users.noreply.github.com>
2025-10-08 16:58:26 +0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,51 +2,59 @@
   description = "Flake BTW";
 
   inputs = {
-    # Essentials (nixpkgs-channel and home-manager)
+    
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # Flake imports:
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+   
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, zen-browser, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      username = "sicsick";
-    in {
-      nixosConfigurations.emptiness = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager, zen-browser, nixvim, ... }@inputs: 
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+     nixosConfigurations.emptiness = nixpkgs.lib.nixosSystem {
+        
         specialArgs = {
           inherit inputs;
-          inherit system;
-        };
+	};
+        
         modules = [
           ./nixos/configuration.nix
-
-          {
-            nixpkgs.overlays = [
-            ];
-          }
-
-          # Home Manager as a NIXOS Module
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              users.sicsick = import ./home-manager/home.nix;
-              #useGlobalPkgs = true;
-              useUserPackages = true;
-              
-	      # Home-Manager Modules
-              extraSpecialArgs = {
-                inherit inputs; 
-              };
-              sharedModules = [
-              ];
-            };
-          }
+	  inputs.nixvim.nixosModules.nixvim
+          #{
+          #  nixpkgs.overlays = [
+          #  ];
+          #}
+	];
+      };
+         
+      homeConfigurations.sicsick = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+	      extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+	        ./home-manager/home.nix
+	      ];
+      };
+      
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+  
         ];
       };
     };
